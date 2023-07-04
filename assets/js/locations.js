c0fdeb25528093592517c226448ed2dd3f6691b7
Yusuf Çelmez <72373760+celmez@users.noreply.github.com>
2023-07-04 14:04:28 +0300

    Update locations.js

--- assets/js/locations.js
+++ assets/js/locations.js
@@ -1,3 +1,3 @@
 const logo = document.getElementById('celmez-item-logo-a')
 
-logo.addEventListener('click', () => { window.location.href = 'http://127.0.0.1:5500' })
+logo.addEventListener('click', () => { window.location.href = 'https://www.celmez.com/' })
